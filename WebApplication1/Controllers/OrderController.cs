647897b0cc3bfbeec4c1c5b89691db6f936de83c
1102137126 <1102137126@gm.kuas.edu.tw>
2016-03-17 15:48:22 +0800

    0317

---
+++ WebApplication1/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        /// <summary>
+        /// 訂單管理系統
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("11111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單的畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單存檔的Action
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
+        [HttpGet()]
+        public JsonResult TestJson()
+        {
+            var result = new Models.Order() { CustId = "1102137103", CustName = "阿涵" };
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/OrderService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder(Models.Order order)
+        {
+        }
+        /// <summary>
+        /// 刪除訂單 By Id
+        /// </summary>
+        public void DeleteOrderById(String id)
+        {
+        }
+        /// <summary>
+        /// 修改訂單
+        /// </summary>
+        public void UpdateOrder()
+        {
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <param name="id">訂單ID</param>
+        /// <returns></returns>
+        public Models.Order GetOrderById(string id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "1102137103";
+            result.CustName = "阿涵";
+            return result;
+            //return new Models.Order();
+        }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+    }
+}
